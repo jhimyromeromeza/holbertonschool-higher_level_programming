112591c1b30a1960f653c8a43b4d3729f6e89a21
jhimyromeromeza <romeromezajhimy0@gmail.com>
2023-07-16 11:15:52 -0700

    import function add, sub. mul, div

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+
+a = 10
+b = 5
+
+resultado_add = add(a, b)
+resultado_sub = sub(a, b)
+resultado_mul = mul(a, b)
+resultado_div = div(a, b)
+
+print("{} + {} = {}".format(a, b, resultado_add))
+print("{} - {} = {}".format(a, b, resultado_sub))
+print("{} * {} = {}".format(a, b, resultado_mul))
+print("{} / {} = {}".format(a, b, resultado_div))
