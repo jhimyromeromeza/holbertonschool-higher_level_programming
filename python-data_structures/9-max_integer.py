d896ea5d1ce133040a756064687c95d783df0b11
jhimyromeromeza <romeromezajhimy0@gmail.com>
2023-07-16 19:32:54 -0700

    look max in an list

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    mayor = my_list[0]
+    for i in my_list:
+        if i > mayor:
+            mayor = i
+    return mayor
