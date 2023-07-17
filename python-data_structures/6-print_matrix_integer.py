ab08fcf2a22e2882709e87ced61b7412ea33f642
jhimyromeromeza <romeromezajhimy0@gmail.com>
2023-07-16 19:08:57 -0700

    print list of three

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
-        array = ""
-        for j in i:
-            array += str(j) + " "
-        print(array.strip())
+        print(" ".join("{:d}".format(element)for element in i))
