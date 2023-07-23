96b3476381d0e3a7553e70c9686e0bf4a5f0d849
jhimyromeromeza <romeromezajhimy0@gmail.com>
2023-07-23 16:46:29 -0700

    print numbe int if not is number continue

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -3,7 +3,7 @@ def safe_print_list_integers(my_list=[], x=0):
     nb_print = 0
     for i in range(x):
         try:
-            print("{:d}".format(my_list[i]))
+            print("{:d}".format(my_list[i]), end="")
             nb_print += 1
         except (ValueError, TypeError):
             continue
