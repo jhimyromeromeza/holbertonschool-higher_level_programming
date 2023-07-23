dbccbd8408091667917e4523107c5f01642d1154
jhimyromeromeza <romeromezajhimy0@gmail.com>
2023-07-23 16:42:48 -0700

    print numbers int if not is continue

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         for i in range(x):
             print(my_list[i], end="")
-            acumulador = |acumulador + 1
+            acumulador = acumulador + 1
     except IndexError:
         pass
     print()
---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    nb_print = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]))
+            nb_print += 1
+        except (ValueError, TypeError):
+            continue
+    print()
+    return nb_print
