40ff3343056a91591146e8127a21eb70af027c22
jhimyromeromeza <romeromezajhimy0@gmail.com>
2023-07-23 21:48:14 -0700

    inicialicicion

---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" class square."""
+
+
+class Square:
+    """class que define un square"""
+
+    def __init__(self, size):
+        """metodo de inicializacion de la clase
+        Ags:
+            size(int): tamaños de cuadrado
+        """
+        self.__size = size
