6e5e6b821b59e333da708a585cf4ce5579cae2af
jhimyromeromeza <romeromezajhimy0@gmail.com>
2023-07-23 19:30:13 -0700

    execute of manner safe

---
+++ python-exceptions/101-safe_function.py
@@ -0,0 +1,8 @@
+def safe_function(fct, *args):
+    import sys
+    try:
+        valor = fct(*args)
+    except Exception as e:
+        print("Exception: {}".format(e), file=sys.stderr)
+        valor = None
+    return valor
