efdc2348467552668285999133aab673b89bd537
jhimyromeromeza <romeromezajhimy0@gmail.com>
2023-07-23 20:43:10 -0700

    class type

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,6 @@
-python3 -c 'print(__import__("my_module").MyClass.__doc__)'
 #!/usr/bin/python3
+    """ class square: def type of date.
+    """
 class Square:
     """this is an class Square and pass for not value.
 
