a6f7bb51f1355b30bb1791cde369f063f60b9b94
jhimyromeromeza <romeromezajhimy0@gmail.com>
2023-07-23 18:03:12 -0700

    pass

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-python3 -c 'print(__import__("my_module").MyClass.__doc__)'
 class Square:
     """this is an class Square and pass for not value.
     
+    python3 -c 'print(__import__("my_module").MyClass.__doc__)'
     for know the typ of date.
     """
     pass
