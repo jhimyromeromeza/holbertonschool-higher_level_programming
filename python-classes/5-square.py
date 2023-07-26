98c7c17e11f4c9d9b9e08f813ca984489c2c4670
jhimyromeromeza <romeromezajhimy0@gmail.com>
2023-07-25 17:05:23 -0700

    function print square

---
+++ python-classes/5-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""class square"""
+
+
+class Square:
+    """class square definir a square"""
+
+    def __init__(self, size):
+        """metodo que define un square"""
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for i in range(self.__size):
+                    print("#", end="")
+                print()
