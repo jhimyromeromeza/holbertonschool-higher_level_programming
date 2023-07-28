8eaff33bd15c647ed3db4740f07aca746e9bdbb9
jhimyromeromeza <romeromezajhimy0@gmail.com>
2023-07-28 11:49:41 -0700

    Return True or False isinstance

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -5,4 +5,4 @@
 def inherits_from(obj, a_class):
     """return True or False"""
 
-    return issubclass(type(obj), a_class) or type(obj) =! a_class
+    return issubclass(type(obj), a_class) and type(obj) =! a_class
