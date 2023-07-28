29a6534eba5dec184c32f1fc451ee7d2eb9f5a8a
jhimyromeromeza <romeromezajhimy0@gmail.com>
2023-07-28 16:53:57 -0700

    class squeare str

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""class Rectangle"""
+
+
+class BaseGeometry:
+    """metodo instacia public"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value < 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """metodo de instancia __init__"""
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """metodo calcular area"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """metodo para imprimir"""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+
+class Square(Rectangle):
+    """metodo de instancia Square"""
+
+    def __init__(self, size):
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    def __str__(self):
+        return "[Square] {}/{}".format(self._Rectangle__width, self._Rectangle__height)
