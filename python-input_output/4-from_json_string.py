fb89a7827969613ad24732f98a0b9cf5643370a4
jhimyromeromeza <romeromezajhimy0@gmail.com>
2023-07-29 10:21:14 -0700

    forn string to object

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""function of string format JSON to objt"""
+
+
+import json
+
+def from_json_string(my_str):
+    """retur string json to object"""
+
+    return json.loads(my_str)
