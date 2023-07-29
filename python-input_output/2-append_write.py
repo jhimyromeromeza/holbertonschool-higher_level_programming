2236f865ea59061b55e71dfa13cc44ce459198db
jhimyromeromeza <romeromezajhimy0@gmail.com>
2023-07-29 10:10:08 -0700

    return object on format json

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,9 +1,9 @@
-#!/usr/bin/python3
+#!/usr/bin/python3i
 """function append to file"""
 
 
 def append_write(filename="", text=""):
     """mode append text to an file"""
 
-    with open (filename, "a", encoding="utf-8") as a:
+    with open(filename, "a", encoding="utf-8") as a:
         return a.write(text)
---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""function para conbertir object en formato JSON"""
+
+
+import json
+def to_json_string(my_obj):
+    """return in format JSON"""
+
+    return json.dumps(my_obj)
