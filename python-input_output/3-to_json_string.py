799f6d26ae66f9f7fe381b79cf7bb60dbee7c351
jhimyromeromeza <romeromezajhimy0@gmail.com>
2023-07-29 10:31:25 -0700

    from json

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """function for changes the objet to sting json"""
 
+
 import json
+
 
 def to_json_string(my_obj):
     """return in format JSON"""
--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """function of string format JSON to objt"""
 
+
 import json
+
 
 def from_json_string(my_str):
     """retur string json to object"""
