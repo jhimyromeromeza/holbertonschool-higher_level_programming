41b9f850845b8f16c58a702cfbab527264cb83be
jhimyromeromeza <romeromezajhimy0@gmail.com>
2023-07-29 17:33:22 -0700

    document

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -3,8 +3,8 @@
 
 
 from os import path
-import sys
 import json
+import sys
 
 
 def save_to_json_file(my_obj, filename):
@@ -13,11 +13,13 @@ def save_to_json_file(my_obj, filename):
     with open(filename, "w") as f:
         json.dump(my_obj, f)
 
+
 def load_from_json_file(filename):
     """return of filename to object"""
 
     with open(filename, "r") as f:
         return json.load(f)
+
 
 if __name__ == "__main__":
     filename = "add_item.json"
