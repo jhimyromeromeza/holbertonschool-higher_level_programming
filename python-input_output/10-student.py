36b9fc04fee709462373cbc3e80ee2d7e3df1840
jhimyromeromeza <romeromezajhimy0@gmail.com>
2023-07-29 18:05:07 -0700

    return list of tha class Student

---
+++ python-input_output/10-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""class Student"""
+
+
+class Student:
+    """metodo de instnacia init"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """return list of atributes and value"""
+
+        if attrs is None:
+            return self.__dict__
+        else:
+            deposito = {}
+            for i in attrs:
+                if hasattr(self, i):
+                    deposito[i] = getattr(self, i)
+
+            return deposito
