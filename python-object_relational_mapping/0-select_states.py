26d9c0ee15718052d6a8f7f88af4104583de3fe8
jhimyromeromeza <romeromezajhimy0@gmail.com>
2023-08-27 19:48:02 -0700

    Lista todos los estados de la base de datos hbtn_0e_0_usa

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -10,12 +10,10 @@ if __name__ == '__main__':
     Access to the database and get the states
     from the database.
     """
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
-                        db=sys.argv[3], port=3306)
-
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                        passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states;")
-    states = cur.fetchall()
-
-    for state in states:
-        print(state)
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
