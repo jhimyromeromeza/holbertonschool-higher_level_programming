c83b29ac700cf5f72084a6ac0bf69e6a07bcbd4b
jhimyromeromeza <romeromezajhimy0@gmail.com>
2023-08-27 19:18:36 -0700

    correct code

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -2,10 +2,9 @@
 -- shows linked to each.
 -- Does not display genres without linked shows.
 -- Records are ordered by descending number of shows linked.
-SELECT g.`name` AS `genre`,
-       COUNT(*) AS `number_of_shows`
-  FROM `tv_genres` AS g
-       INNER JOIN `tv_show_genres` AS t
-       ON g.`id` = t.`genre_id`
- GROUP BY g.`name`
- ORDER BY `number_of_shows` DESC;
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title;
+
