53414ab8486493f622d63c35a692f72ec550fbc8
jhimyromeromeza <romeromezajhimy0@gmail.com>
2023-10-20 08:46:55 -0500

    exercies of 0 to 4

---
+++ javascript-web_jquery/2-script.js
@@ -0,0 +1,4 @@
+// cambiar de color rojo cuando se haga click en red_header
+$('#red_header').click(function() {
+    $('header').css('color', '#FF0000')
+})
---
+++ javascript-web_jquery/3-script.js
@@ -0,0 +1,4 @@
+// cambiar el color de header agregando un clase red 
+$('#red_header').click(function() {
+    $('header').addClass('red')
+})
